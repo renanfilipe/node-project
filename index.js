5f21bff606de518e174a1bb5cd5baaf71a1559e8
renanfilipe <renanfilipe10@hotmail.com>
2018-12-18 06:21:41 -0200

    end of section 4

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
 require("./services/passport");
 
+mongoose.connect(
+  keys.mongoURI,
+  { useNewUrlParser: true },
+  function(err) {
+    if (err) {
+      console.log("error: ", err);
+    }
+  }
+);
+
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
