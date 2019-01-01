0fe19aa699d0eb6db19a1f4eff2990a97ab0fc9c
renanfilipe <renanfilipe10@hotmail.com>
2019-01-01 19:17:13 -0200

    end of section 10

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const passport = require("passport");
 const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 mongoose.connect(
   keys.mongoURI,
   { useNewUrlParser: true },
-  function(err) {
+  err => {
     if (err) {
       console.log("error: ", err);
     }
@@ -31,6 +32,7 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
---
+++ services/Mailer.js
@@ -0,0 +1,24 @@
+const sgMail = require("@sendgrid/mail");
+const keys = require("../config/keys");
+
+class Mailer {
+  constructor({ subject, recipients }, content) {
+    this.message = {
+      to: recipients,
+      from: "no-reply@emaily.com",
+      subject: subject,
+      html: content,
+      trackingSettings: {
+        clickTracking: { enable: true }
+      }
+    };
+    sgMail.setApiKey(keys.sendGridKey);
+  }
+
+  async send() {
+    const response = await sgMail.sendMultiple(this.message);
+    return response;
+  }
+}
+
+module.exports = Mailer;
